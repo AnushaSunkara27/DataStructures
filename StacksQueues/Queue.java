730ee0b10f0c9e9b54ea7f1f7eb855e3fa32860b
AnushaSunkara27 <anusha.vjcf1@gmail.com>
2023-10-19 21:51:43 -0500

    Implementation for stack and queue operations

---
+++ StacksQueues/Queue.java
@@ -0,0 +1,72 @@
+package StacksQueues;
+
+public class Queue {
+    private Node first;
+    private Node last;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        length = 1;
+    }
+
+    public void printQueue() {
+        Node temp = first;
+        System.out.println("\n");
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+
+    }
+
+    public void enqueue(int value) {
+        Node newNode = new Node(value);
+
+        if(length == 0) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+        length++;
+    }
+
+    public Node dequeue() {
+        Node temp = first;
+        if(length == 0) return null;
+        if(length == 1) {
+            first = null;
+            last = null;
+        } else {
+            first = first.next;
+            temp.next = null;
+        }
+        length--;
+        return temp;
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(1);
+        queue.printQueue();
+
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.printQueue();
+
+        queue.dequeue();
+        queue.printQueue();
+    }
+}
