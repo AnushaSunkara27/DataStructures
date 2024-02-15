c9e970f2a03872d896aac728cbe0be7bef4e8382
AnushaSunkara27 <anusha.vjcf1@gmail.com>
2024-02-15 09:40:55 -0600

    Grapg bfs and dfs

--- Graphs/Graph.java
+++ Graphs/Graph.java
@@ -2,6 +2,9 @@ package Graphs;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Graph {
     HashMap<String, ArrayList<String>> adjacencyList 
@@ -46,6 +49,56 @@ public class Graph {
         return true;
     }
 
+    // Breadth-First Search (BFS)
+    public void bfs(String startVertex) {
+        if (!adjacencyList.containsKey(startVertex))
+            return;
+
+        Queue<String> queue = new LinkedList<>();
+        ArrayList<String> visited = new ArrayList<>();
+
+        queue.add(startVertex);
+        visited.add(startVertex);
+
+        while (!queue.isEmpty()) {
+            String currentVertex = queue.poll();
+            System.out.print(currentVertex + " ");
+
+            for (String neighbor : adjacencyList.get(currentVertex)) {
+                if (!visited.contains(neighbor)) {
+                    queue.add(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    // Depth-First Search (DFS)
+    public void dfs(String startVertex) {
+        if (!adjacencyList.containsKey(startVertex))
+            return;
+
+        Stack<String> stack = new Stack<>();
+        ArrayList<String> visited = new ArrayList<>();
+
+        stack.push(startVertex);
+        visited.add(startVertex);
+
+        while (!stack.isEmpty()) {
+            String currentVertex = stack.pop();
+            System.out.print(currentVertex + " ");
+
+            for (String neighbor : adjacencyList.get(currentVertex)) {
+                if (!visited.contains(neighbor)) {
+                    stack.push(neighbor);
+                    visited.add(neighbor);
+                }
+            }
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         Graph graph = new Graph();
 
@@ -55,8 +108,16 @@ public class Graph {
         graph.printGraph();
 
         graph.addEdge("A", "C");
+        graph.addEdge("A", "B");
+
 
         graph.printGraph();
+
+        System.out.print("BFS starting from 'A': ");
+        graph.bfs("A");
+
+        System.out.print("DFS starting from 'A': ");
+        graph.dfs("A");
     }
 
     
---
+++ Graphs/GraphOperations.java
@@ -0,0 +1,23 @@
+package Graphs;
+
+class Edge {
+    int src;
+    int dest;
+
+    Edge(int src, int dest) {
+        this.src = src;
+        this.dest = dest;
+    }
+}
+
+public class GraphOperations {
+    
+    static class Vertex {
+        int v;
+
+        Vertex(int v) {
+            this.v = v;
+        }
+    }
+    
+}
